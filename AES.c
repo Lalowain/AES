0e474362b258a0f2ad1bdee849481d3cd1abda60
manals <malmazrua@kacst.edu.sa>
2015-06-01 07:35:45 +0300

    Update AES.c

--- AES.c
+++ AES.c
@@ -3,27 +3,30 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <conio.h>
+#include <windows.h>
+#include <Math.h>
 #include "AES.h"
 
 
 
-
-void Add_key(unsigned char state[4][4], unsigned char temp_key[4][4])
+void Add_key(unsigned char state[4][4], unsigned char temp_key[4][44],int index)
 {
 	int i,j;
 
   for(i = 0; i < 4; i++) {
     for(j = 0; j < 4; j++) {
-      state[i][j] = state[i][j] ^ temp_key[i][j];
+      state[i][j] = state[i][j] ^ temp_key[i][index+j];
     }
   }
 }
+
 
 void key_generate(unsigned char key[4][4], unsigned char expan_key[4][44])
 {
 	int i,j;
   int rloop = 0;
-  printf("\n RCON \n");
+
   for(i = 0; i < 44; i++)
   {
     if(i < 4)
@@ -65,19 +68,7 @@ void key_generate(unsigned char key[4][4], unsigned char expan_key[4][44])
     }
   }
 
-  printf("\n");
 
-  printf("\n Key expansion \n");
-
-  printf("\n");
-
-  for(i = 0; i < 4; i++){
-    for(j = 0; j < 44; j++){
-      printf("%#X",expan_key[i][j]);
-      printf("\t");
-    }
-    printf("\n");
-  }
 }
 
 
@@ -94,6 +85,22 @@ void subByte(unsigned char state[4][4])
 
 
 }
+
+
+void rev_subbytes(unsigned char state[4][4])
+{
+	int i,j;
+  for(i=0;i<4;i++){
+    for(j=0;j<4;j++){
+
+      state[i][j]=rsbox[state[i][j]];
+    }
+  }
+
+
+
+}
+
 
 void shiftRaw(unsigned char state[4][4])
 {
@@ -118,6 +125,32 @@ void shiftRaw(unsigned char state[4][4])
   state[3][3]=state[3][2];
   state[3][2]=state[3][1];
   state[3][1]=tmp;
+
+}
+
+void rev_shiftrows(unsigned char state[4][4])
+{
+
+
+  char tmp;
+  tmp=state[3][3] ;
+   state[3][3]=state[3][0];
+   state[3][0]=state[3][1];
+   state[3][1]=state[3][2];
+   state[3][2]=tmp;
+
+   tmp=state[2][0] ;
+   state[2][0]=state[2][2];
+   state[2][2]=tmp;
+   tmp=state[2][3];
+   state[2][3]=state[2][1];
+   state[2][1]=tmp;
+
+   tmp=state[1][0] ;
+   state[1][0]=state[1][3];
+   state[1][3]=state[1][2];
+   state[1][2]=state[1][1];
+   state[1][1]=tmp;
 
 }
 
@@ -146,8 +179,8 @@ void mixcolumn(unsigned char state[4][4], unsigned char temp[4][4])
       for(k=0;k<4;k++)
       {
 
-         
           tem =((state[k][i]<< (coumn[j][k]>>1))^(state[k][i]*((coumn[j][k]>>1)&coumn[j][k])))^(((state[k][i]>>7)*0x1b )*(coumn[j][k]>>1)) ;
+
 
         temp[j][i] = temp[j][i]^(tem);
 
@@ -157,6 +190,121 @@ void mixcolumn(unsigned char state[4][4], unsigned char temp[4][4])
   }
 
 
+
+  for(i=0;i<4;i++)
+      for(j=0;j<4;j++)
+        state[i][j]=temp[i][j];
+
+}
+
+
+void rev_mixcolumn(unsigned char state[4][4], unsigned char temp[4][4])
+{
+
+
+	int i,j,k;
+
+  unsigned char coumn[4][4]={{0x0E, 0x0B, 0x0D, 0x09},
+    {0x09, 0x0E, 0x0B, 0x0D},
+    {0x0D, 0x09, 0x0E, 0x0B},
+    {0x0B, 0x0D, 0x09, 0x0E}};
+
+  unsigned  char BYTE_LEFT_BIT = 0x80;
+  unsigned  char BYTE_Right_BIT = 0x01;
+
+
+  		 unsigned  char a,b=0x00;
+  for(i=0;i<4;i++)
+    for(j=0;j<4;j++)
+      temp[i][j]=0x00;
+
+  unsigned  char tem=0x00;
+
+  for(j=0;j<4;j++)
+  {
+
+    for (i=0;i<4;i++)
+    {
+
+      for(k=0;k<4;k++)
+      {
+    	  tem=0x00;
+            b=state[k][i];
+            a=coumn[j][k];
+
+    	  			tem =(b*(a & BYTE_Right_BIT))^(((b =((((b<<1)^(0x1b*((b & BYTE_LEFT_BIT)/BYTE_LEFT_BIT)))^((a>>=1)/0xff))))*(a & BYTE_Right_BIT)))^(((b =((((b<<1)^(0x1b*((b & BYTE_LEFT_BIT)/BYTE_LEFT_BIT)))^((a>>=1)/0xff))))*(a & BYTE_Right_BIT)))^(((b =((((b<<1)^(0x1b*((b & BYTE_LEFT_BIT)/BYTE_LEFT_BIT)))^((a>>=1)/0xff))))*(a & BYTE_Right_BIT)));
+
+    	  		//	((b =((((b<<1)^(0x1b*((b & BYTE_LEFT_BIT)/BYTE_LEFT_BIT)))^((a>>=1)/0xff))))*(a & BYTE_Right_BIT));// b mod x^4+x^3+x+1
+
+        temp[j][i] = temp[j][i]^(tem);
+
+      }
+
+    }
+  }
+
+
+  for(i=0;i<4;i++)
+      for(j=0;j<4;j++)
+        state[i][j]=temp[i][j];
+
+}
+
+void encryption(unsigned char state[4][4], unsigned char expan_key[4][44])
+{
+
+	Add_key(state,expan_key,0);
+
+
+	int start=4;
+	int loop;
+	unsigned char temp[4][4];
+
+	for(loop=1;loop<10;loop++)
+	{
+
+	subByte(state);
+
+	shiftRaw(state);
+
+	mixcolumn(state,temp);
+
+
+	Add_key(state,expan_key,(int)(loop*start));
+
+	}
+
+	subByte(state);
+	shiftRaw(state);
+	Add_key(state,expan_key,(int)(loop*start));
+
+
+}
+
+void decryption(unsigned char state[4][4], unsigned char expan_key[4][44])
+{
+	int start=4;
+		int loop;
+		unsigned char temp[4][4];
+
+		Add_key(state,expan_key,(40));
+
+
+		for(loop=9;loop>0;loop--)
+		{
+
+			rev_shiftrows(state);
+		    rev_subbytes(state);
+
+			Add_key(state,expan_key,(loop*start));
+
+		    rev_mixcolumn(state,temp);
+
+			}
+
+		rev_shiftrows(state);
+		rev_subbytes(state);
+		Add_key(state,expan_key,(0));
 }
 
 void print(unsigned char print[4][4])
@@ -174,3 +322,18 @@ void print(unsigned char print[4][4])
   printf("\n");
 }
 
+
+void prints(unsigned char print[4][44],int index)
+{
+	int i,j;
+  for(i=0;i<4;i++){
+    for(j=0;j<4;j++){
+
+      printf("%#X",print[i][index+j]);
+      printf("\t");
+
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
